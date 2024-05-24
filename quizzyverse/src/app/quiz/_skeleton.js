fc279de3ddefe7cf9231d3c78f64511c39a44066
AlexandreWilliam <marc-alexandre.lepinard@epitech.eu>
2024-05-24 12:23:35 +0200

    MOD quiz_skeleton front v1
    	new file:   quizzyverse/src/app/quiz/_skeleton.css
    	modified:   quizzyverse/src/app/quiz/_skeleton.js

--- quizzyverse/src/app/quiz/_skeleton.js
+++ quizzyverse/src/app/quiz/_skeleton.js
@@ -1,27 +1,31 @@
 "use client"
 
 import React, { useState } from 'react'
+import "./_skeleton.css"
 
 const QuizContainer = () => {
 
   const initialValue = {
     quiz_title: "Quiz #",
-    question: "Quel est le titre de ce flim / d'où vient ce truc / quoi c'est ? etc.",
+    question: "Quel est le titre de ce flim / d'où vient ce truc / quoi c'est ça ? etc.",
     question_number: 1,
     total_number: 10,
     type: 'image',
     imageUrl: "https://m.media-amazon.com/images/I/71CsUmAR-GL._SL1500_.jpg",
     imageAlt: "",
-    show_indice: false,
-    indice: "C'est poilu"
+    show_clue: false,
+    clue: "C'est poilu",
+    clue_price: 1,
+    response_type: "text",
   }
 
   const [myState, setMyState] = useState(initialValue)
 
   //setMyState({imageUrl: 'https://m.media-amazon.com/images/I/71CsUmAR-GL._SL1500_.jpg'});
 
-  const handleIndice = (event) => {
-    console.log(initialValue.indice);
+  const handleClue = () => {
+    console.log(initialValue.clue);
+    setMyState({...myState, show_clue: true})
   }
 
   const handleEvent = (event) => {
@@ -30,17 +34,20 @@ const QuizContainer = () => {
 
   return (
 
-    <div style={{ textAlign: 'center' }}>
+    <div style={{ textAlign: 'center', display: "" }}>
+
         {/*rend les components en fonction de ce qu'on décide de mettre dans 'const initialValue'*/}
         <h1 style={{fontSize: '48px'}}>{myState.quiz_title}</h1>
+        <br/>
         <h3 style={{fontSize: '24px'}}>{myState.question}</h3>
+        <br/>
 
         {/*si quiz de type 'image'...*/}
         {myState.type === 'image' && (
         <>
             <img style={{ display: 'inline-block' }} src={myState.imageUrl} alt={myState.imageAlt} width={250} height={"auto"} />
             <p>
-                <br/>
+                <br/><br/>
                 Question {myState.question_number} of {myState.total_number}
             </p>
         </>
@@ -49,18 +56,19 @@ const QuizContainer = () => {
         {myState.type === 'text' && (
          "I'm a freeking text"
          )}
-        
-        {myState.show_indice === true ? (
-            <>
-            Clue: {myState.indice}
-            </>
+        <br/>
+        {myState.response_type === 'text' && (
+          <input style={{width: 200, border: "solid", borderWidth: "1px", borderRadius: "10px", marginTop: "20px", marginBottom: "50px", padding: "0 5px"}} placeholder="Try your best..." />
+        )}
+        <br/>
+        {myState.show_clue === true ? (
+            <span>Clue: {initialValue.clue}</span>
             )
             :
-            <button style={{fontStyle: "italic"}} onClick={{handleIndice}}>"pay for a clue !"</button>
+            <button style={{fontStyle: "italic", border: "solid", borderWidth: "1px", borderRadius: "10px", padding: "0 15px"}} onClick={handleClue}>Pay {"{"}clue_price{"}"} xp for a clue !</button>
         }
-
-
-        {/*si quiz de type 'video'...*/}
+         <button style={{fontStyle: "italic", border: "solid", borderWidth: "1px", borderRadius: "10px", marginLeft: "50px", marginBottom: "50px", padding: "5px 15px", backgroundColor: "#FFD700"}} onClick={handleClue} >Answer</button>
+         <br/>
     </div>
   )
 }
