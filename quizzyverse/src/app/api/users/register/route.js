6948ac1d19730802463fdc70f62c86b1ded37295
AlexandreWilliam <marc-alexandre.lepinard@epitech.eu>
2024-05-27 01:17:26 +0200

    UPD Register completed (or almost, small updates may still happen like error handling) - country is selectable by a drop-down menu - password must be confirmed - password is hashed (in the back) - user is redirected ('/') when registration is done
    	modified:   quizzyverse/.gitignore
    	modified:   quizzyverse/package-lock.json
    	modified:   quizzyverse/package.json
    	new file:   quizzyverse/src/app/api/users/register/route.js
    	modified:   quizzyverse/src/app/register/page.js
    	modified:   quizzyverse/src/app/register/register.css

---
+++ quizzyverse/src/app/api/users/register/route.js
@@ -0,0 +1,71 @@
+import {NextResponse} from "next/server"
+
+import mysql from 'mysql2/promise'
+const pool = mysql.createPool({host: process.env.MYSQL_HOST, user: process.env.MYSQL_USER, password: process.env.MYSQL_PASSWORD, database: process.env.MYSQL_DATABASE, waitForConnections: true})
+import bcrypt from 'bcrypt'
+
+//////////////////////
+// ===> Get user list
+export async function GET() {
+
+	let users;
+	try {
+    const sql = 'SELECT * FROM users;'
+    const mySqlResponse = await pool.query(sql)
+	users = mySqlResponse[0]
+
+	if (false) {throw "Erreur"} // <=== à gérer
+
+	} catch (error) {
+		console.error(error)
+		return new NextResponse(error, {
+			statut : 204,
+		});
+	}
+
+    return NextResponse.json({
+        users,
+    });
+}
+
+//////////////////////////
+// ===> Register new user
+export async function POST(request, context) { //in TS: POST(request: Request)
+    const { body } = request
+    console.log("\n\n\n\n\n");
+    console.log("BODY ===========>", body);
+    const data  = await request.json()
+    console.log(data);
+    if (data.username === '') {data.username = null}
+    if (data.country === '') {data.country = null}
+    if (data.email === '') {data.email = null}
+    if (data.password === '') {data.password = null}
+    console.log(data.username);
+    console.log(data.country);
+    console.log(data.email)
+    console.log(data.password)
+    console.log("\n\n\n\n\n");
+    const hashedPassword = await bcrypt.hash(data.password, 10)
+    try {
+        const sql = 'INSERT INTO users (id, is_admin, username, country, email, password, xp, is_premium, daily_count) VALUES (NULL, 0, ?, ?, ?, ?, 0, 0, 0);'
+        const mySqlResponse = await pool.query(sql, [data.username, data.country, data.email, hashedPassword])
+        console.log("\n\n\n\n\n")
+        console.log(mySqlResponse);
+        console.log("\n\n\n\n\n")
+        /*
+        if (rows.length > 0) {
+          res.status(200).json({ message: 'User registered' })
+        } else {
+          res.status(401).json({ message: 'Invalid email or password' })
+        }*/
+    } catch (error) { //<==== à gérer
+        console.error(error)
+        return new NextResponse(error, {
+            statut : 500,
+        });
+    }
+
+    return NextResponse.json({
+        message: "User registered",
+    });
+}
--- quizzyverse/src/app/register/page.js
+++ quizzyverse/src/app/register/page.js
@@ -9,17 +9,60 @@ const RegisterForm = () => {
   const [email, setEmail] = useState('')
   const [country, setCountry] = useState('')
   const [password, setPassword] = useState('')
+  const [passwordConfirmation, setPasswordConfirmation] = useState('')
   //const [xp, setXp] = useState(0)
   //const [is_premium, setIsPremium] = useState(false)
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async(event) => {
     event.preventDefault()
 
     console.log(username)
     console.log(country);
     console.log(email);
     console.log(password);
+    console.log(passwordConfirmation);
+
+    if (password !== passwordConfirmation) {
+      alert("The password and its confirmation do not match.\nPlease enter your password again.")
+      setPassword('')
+      setPasswordConfirmation('')
+      return -1;
+    } else if (country === "") {
+      alert("Please select a country.")
+      return -1;
+    }
+
+    const data = {
+      username,
+      country,
+      email,
+      password
+    }
+  
+    try {
+      const response = await fetch('/api/users/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
+  
+      if (response.ok) {
+        console.log("User registered");
+        console.log(response);
+        window.location.href = '/'
+      } else {
+        console.log("There was an error registering the user")
+        console.log(response);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+
   }
+
+  const countries = [ 'Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola', 'Antigua & Deps', 'Argentina', 'Armenia', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bhutan', 'Bolivia', 'Bosnia Herzegovina', 'Botswana', 'Brazil', 'Brunei', 'Bulgaria', 'Burkina', 'Burundi', 'Cambodia', 'Cameroon', 'Canada', 'Cape Verde', 'Central African Rep', 'Chad', 'Chile', 'China', 'Colombia', 'Comoros', 'Congo, Democratic Republic of the', 'Congo, Republic of the', 'Costa Rica', 'Croatia', 'Cuba', 'Cyprus', 'Czech Republic', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 'East Timor', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 'Eritrea', 'Estonia', 'Ethiopia', 'Fiji', 'Finland', 'France', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana', 'Greece', 'Grenada', 'Guatemala', 'Guinea', 'Guinea-Bissau', 'Guyana', 'Haiti', 'Honduras', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran', 'Iraq', 'Ireland', 'Israel', 'Italy', 'Ivory Coast', 'Jamaica', 'Japan', 'Jordan', 'Kazakhstan', 'Kenya', 'Kiribati', 'Korea North', 'Korea South', 'Kosovo', 'Kuwait', 'Kyrgyzstan', 'Laos', 'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Macedonia', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Marshall Islands', 'Mauritania', 'Mauritius', 'Mexico', 'Micronesia', 'Moldova', 'Monaco', 'Mongolia', 'Montenegro', 'Morocco', 'Mozambique', 'Myanmar, Burma', 'Namibia', 'Nauru', 'Nepal', 'Netherlands', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'Norway', 'Oman', 'Pakistan', 'Palau', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Poland', 'Portugal', 'Qatar', 'Romania', 'Russian Federation', 'Rwanda', 'St Kitts & Nevis', 'St Lucia', 'Saint Vincent & the Grenadines', 'Samoa', 'San Marino', 'Sao Tome & Principe', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles', 'Sierra Leone', 'Singapore', 'Slovakia', 'Slovenia', 'Solomon Islands', 'Somalia', 'South Africa', 'South Sudan', 'Spain', 'Sri Lanka', 'Sudan', 'Suriname', 'Swaziland', 'Sweden', 'Switzerland', 'Syria', 'Taiwan', 'Tajikistan', 'Tanzania', 'Thailand', 'Togo', 'Tonga', 'Trinidad & Tobago', 'Tunisia', 'Turkey', 'Turkmenistan', 'Tuvalu', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 'United States', 'Uruguay', 'Uzbekistan', 'Vanuatu', 'Vatican City', 'Venezuela', 'Vietnam', 'Yemen', 'Zambia', 'Zimbabwe' ]
 
   return (
     <>
@@ -33,13 +76,22 @@ const RegisterForm = () => {
         <input style={{ marginBottom: '10px' }} type="text" value={username} onChange={(event) => setUsername(event.target.value)} />
         <br />
         <label style={{ marginBottom: '10px' }}>Country:</label>
-        <input style={{ marginBottom: '10px' }} type="text" value={country} onChange={(event) => setCountry(event.target.value)} />
+        {/*<input style={{ marginBottom: '10px' }} type="text" value={country} onChange={(event) => setCountry(event.target.value)} />*/}
+        <select style={{ marginBottom: '10px' }} type="text" value={country} onChange={(event) => setCountry(event.target.value)} >
+          <option value="" style={{ fontStyle: 'italic' }}>Select a country</option>
+          {countries.map((country) => (
+            <option key={country} value={country}>{country}</option>
+          ))}
+        </select>
         <br />
         <label style={{ marginBottom: '10px' }}>Email:</label>
         <input style={{ marginBottom: '10px' }} type="email" value={email} onChange={(event) => setEmail(event.target.value)} />
         <br />
         <label style={{ marginBottom: '10px' }}>Password:</label>
-        <input style={{ marginBottom: '50px' }} type="password" value={password} onChange={(event) => setPassword(event.target.value)} />
+        <input style={{ marginBottom: '10px' }} type="password" value={password} onChange={(event) => setPassword(event.target.value)} />
+        <br />
+        <label style={{ marginBottom: '10px' }}>Confirm password:</label>
+        <input style={{ marginBottom: '50px' }} type="password" value={passwordConfirmation} onChange={(event) => setPasswordConfirmation(event.target.value)} />
         <br />
     {/* admin panel reserved
         <label style={{ marginBottom: '10px' }}>XP:</label>
