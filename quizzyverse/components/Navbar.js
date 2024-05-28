f21c8968a0f97f9a9d57b43ac0e0b5b179f178b6
Acrazie <mayeul@Acrasie>
2024-05-28 17:12:30 +0200

    Ajout de la NavBar Component

---
+++ quizzyverse/components/Navbar.js
@@ -0,0 +1,31 @@
+import Link from "next/link";
+
+
+export default function NavBar() {
+  return (
+      <nav className="bg-blue-600 p-4 rounded-lg flex justify-between items-center">
+        <div className="left flex space-x-4">
+          <Link className="text-white font-bold" href="/about">
+            About
+          </Link>
+          <Link className="text-white font-bold" href="/xp">
+            XP
+          </Link>
+          <Link className="text-white font-bold" href="/admin">
+            Admin
+          </Link>
+        </div>
+        <div className="center">
+          <Link className="text-white font-bold text-2xl" href='/'>Quizzyverse</Link>
+        </div>
+        <div className="right flex space-x-4">
+          <Link className="text-white font-bold" href="/login-register">
+            Log In
+          </Link>
+          <Link className="text-white font-bold" href="/login-register">
+            Register
+          </Link>
+        </div>
+      </nav>
+  );
+}
