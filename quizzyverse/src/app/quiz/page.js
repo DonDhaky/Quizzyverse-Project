36f0f3e776f625372c3d5ebc177bbd6228598c64
DonDhaky <erick.thonon@epitech.eu>
2024-05-28 14:49:03 +0200

    quizz 80% ok

--- quizzyverse/src/app/quiz/page.js
+++ quizzyverse/src/app/quiz/page.js
@@ -2,6 +2,7 @@
 
 import 'tailwindcss/tailwind.css';
 import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 // Thèmes de quiz
 const quizzFlag = "Trouve le pays qui correspond au drapeau !";
@@ -13,10 +14,18 @@ const quizzFlag = "Trouve le pays qui correspond au drapeau !";
 const quizzTheme = quizzFlag;
 
 const Page = () => {
+    // j'utilise useState pour définir des états de base pour les varaibles venant à être modifiées
     const [fetchedImage, setFetchedImage] = useState('');
     const [fetchedAnswer, setFetchedAnswer] = useState('');
     const [fetchedTip, setFetchedTip] = useState("");
-    const [questionNumber, setQuestionNumber] = useState(3); // Par défaut à 3, peut être modifié selon le besoin
+    const [questionNumber, setQuestionNumber] = useState(1); // Par défaut à 1
+    const [showTip, setShowTip] = useState(false);
+    const [userAnswer, setUserAnswer] = useState(''); // stockage de la réponse du joueur pour comparer
+    const [showResultPopup, setShowResultPopup] = useState(false); // affichage de la pop-up à false par défaut
+    const [resultMessage, setResultMessage] = useState(''); // stockage du message de résultat
+    const [xpWon, setXpWon] = useState(0); // xp gagnée à incrémenter et à ajouter au user dans la DB ultérieurement
+    const router = useRouter();
+
 
     // J'utilise "useEffect" pour récupérer les données de l'api lors du montage du composant
     useEffect(() => {
@@ -24,26 +33,48 @@ const Page = () => {
             try {
                 const response = await fetch('/api/flags');
                 const data = await response.json();
-                // Adapter les champs de data selon les informations récupérées de votre API
-                setFetchedImage(data[0].flags.png);
-                console.log(data[0].flags.png);
-                setFetchedAnswer(data[0].name.common);
-                setFetchedTip(data[0].capital ? `La capitale est ${data[0].capital[0]}` : 'Pas d\'indice disponible');
+                console.log(data);
+
+                // Adapter les champs de data selon l'API !!!!
+                setFetchedImage(data.flags.png);
+                console.log(data.flags.png);
+                setFetchedAnswer(data.translations.fra.common);
+                console.log(data.translations.fra.common);
+                 setFetchedTip(data.capital ? `Capitale(s) de ce pays : ${data.capital}` : 'Pas d\'indice disponible pour ce pays !');
+                console.log(data.capital);
             } catch (error) {
-                console.error('Error while trying to fetch quizz data', error);
+                console.error(error);
             }
         };
 
         fetchQuizzData();
-    }, []);
+    }, [questionNumber]);
 
-    const validateAnswer = () => {
-        console.log("validateAnswer"); // Définir la logique de validation ici
+    const validateAnswer = () => { // bouton de validation de la réponse et affichage du pop-up
+        if (userAnswer.trim().toLowerCase() === fetchedAnswer.trim().toLowerCase()) { // pour la sensibilité à la casse
+            setXpWon(prevXp => prevXp + 10); // Incrémente l'XP si la réponse est correcte
+            setResultMessage('Bonne réponse !');
+        } else {
+            setResultMessage(`Mauvaise réponse, c'était ${fetchedAnswer} !`);
+        }
+        setShowResultPopup(true);
     };
 
-    const showTip = () => {
-        console.log("showTip"); // Définir la logique d'affichage des indices ici
-        setFetchedTip(fetchedTip); // Exemple pour afficher l'indice
+    const handleNextQuestion = () => { // bouton de validation du pop-up et affichage de la nouvelle question
+        setShowResultPopup(false);
+        setUserAnswer(''); // reset de la réponse du joueur
+        setShowTip(false); // on cache de nouveau l'indice
+        setQuestionNumber(prevQuestionNumber => {
+            const nextQuestionNumber = prevQuestionNumber + 1;
+            if (nextQuestionNumber > 5) {
+                router.push('/'); // créer une page pour afficher les résultats finaux si le compteur atteint 6 OU mettre un POP UP DE FIN
+            }
+            return nextQuestionNumber;
+        });
+    };
+
+    const handleShowTip = () => {
+        setShowTip(true); // Met à jour l'état pour afficher l'indice
     };
 
     return (
@@ -53,15 +84,31 @@ const Page = () => {
             <div className="container mx-auto p-4">
                 <div className="max-w-sm mx-auto bg-white shadow-md rounded-md overflow-hidden">
                     <img src={fetchedImage} alt="Quiz Image" className="w-full h-auto max-w-full max-h-full object-contain" />
-                    <div className="text-xl text-center text-black m-3">Question {questionNumber} sur 10</div>
+                    <div className="text-xl text-center text-black m-3">Question {questionNumber} sur 5</div>
                 </div>
             </div>
-            <input name="answer" className="text-xl text-center text-black mt-5 w-80 h-10" type="text"></input>
+            <input 
+                name="answer"
+                className="text-xl text-center text-black mt-5 w-80 h-10"
+                type="text"
+                value={userAnswer}
+                onChange={e => setUserAnswer(e.target.value)}
+            />
             <div className="mt-4">
-                <button type="button" className="mr-20" onClick={showTip}>Indice</button>
+                <button type="button" className="mr-20" onClick={handleShowTip}>Indice</button>
                 <button type="button" className="ml-20" onClick={validateAnswer}>Valider</button>
             </div>
-            <div className="mt-8">{fetchedTip}</div>
+            {showTip && <div className="mt-8">{fetchedTip}</div>}
+            {showResultPopup && (
+                <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75">
+                    <div className="bg-white p-6 rounded shadow-md text-center">
+                        <p className="text-black">{resultMessage}</p>
+                        <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded" onClick={handleNextQuestion}>
+                            Prochaine question
+                        </button>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
