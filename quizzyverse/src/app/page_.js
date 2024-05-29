616b9c4cddbb74e4654103680c8a5c5ed6ad66d6
AlexandreWilliam <marc-alexandre.lepinard@epitech.eu>
2024-05-29 17:33:03 +0200

    UPD Quiz Capitals score system added, bug management
    	modified:   quizzyverse/src/app/api/ressources/Quiz_Capitals/settings.json
    	modified:   quizzyverse/src/app/page_.js
    	modified:   quizzyverse/src/app/quiz/page.js

--- quizzyverse/src/app/page_.js
+++ quizzyverse/src/app/page_.js
@@ -29,7 +29,7 @@ export default function Home() {
         &nbsp;&nbsp;&nbsp;
         <button onClick={(event) => handleQuiz(event)}>Find My Flag</button>
         &nbsp;&nbsp;&nbsp;
-        <button onClick={(event) => handleQuiz(event)}>Quiz Test</button>
+        <button onClick={(event) => handleQuiz(event)}>Quiz Capitals</button>
         &nbsp;&nbsp;&nbsp;
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
--- quizzyverse/src/app/quiz/page.js
+++ quizzyverse/src/app/quiz/page.js
@@ -14,6 +14,10 @@ const QuizContainer = () => {
   const [clue, setClue] = useState(null)
   const [myAnswer, setMyAnswer] = useState('')
 
+  //const [numberOfGoodAnswers, setNumberOfGoodAnswers] = useState(0);
+  let numberOfGoodAnswers = 0;
+  let numberOfRequestedClues = 0;
+
     //voir le contenu de quizSettings dans api/ressources/{quizName}/settings.json
 
     //fonction réservée aux quiz qui ne fonctionnent pas avec le settings.json seul
@@ -32,43 +36,59 @@ const QuizContainer = () => {
     }
 
 
-    useEffect(() => {
-        const quizName = localStorage.getItem('quizName')
-        const getquizSettings = (async() => {
-            const response = await fetch(`/api/ressources/${quizName}`)
-            const data =  await response.json()
-            console.log(data);
-            //initialise le data du quiz ICI:
-            setQuizSettings(data.quizSettings)
-            setQuizQandA(data.QandA_array)
-        })
-        getquizSettings()
-        //myCustomFetch()
-    }, [])
+  useEffect(() => {
+      const quizName = localStorage.getItem('quizName')
+      localStorage.setItem("numberOfGoodAnswers", 0);
+      localStorage.setItem("numberOfRequestedClues", 0);
+      const getquizSettings = (async() => {
+          const response = await fetch(`/api/ressources/${quizName}`)
+          const data =  await response.json()
+          console.log(data);
+          //initialise le data du quiz ICI:
+          setQuizSettings(data.quizSettings)
+          setQuizQandA(data.QandA_array)
+      })
+      getquizSettings()
+      //myCustomFetch()
+  }, [])
 
   const handleClue = () => {
     console.log(quizSettings.clue);
-    setClue(quizQandA[quizSettings.question_number-1][1][0]+"...")
+    setClue(quizQandA[quizSettings.question_number-1][1][0]+"..."+quizQandA[quizSettings.question_number-1][1][quizQandA[quizSettings.question_number-1][1].length-1])
     /*if (quizSettings.show_clue === false) {
       setQuizSettings({...quizSettings, show_clue: true})
     }*/
+    let numberOfRequestedClues = localStorage.getItem('numberOfRequestedClues')
+    numberOfRequestedClues = Number(numberOfRequestedClues)+1
+    localStorage.setItem("numberOfRequestedClues", numberOfRequestedClues)
     setShowClue(true)
   }
 
-  const handleResponse = (event) => {
+  const handleResponse = async(event) => {
     event.preventDefault()
-    //setQuizSettings({...quizSettings, show_clue: false})
-    setShowClue(false)
-    if (quizQandA[quizSettings.question_number-1][1] === myAnswer) {
-      alert(myAnswer+", correct !")
-    } else {
-      alert("Nope... "+quizQandA[quizSettings.question_number-1][1]+", not "+myAnswer+"...")
+      setShowClue(false)
+      if (quizQandA[quizSettings.question_number-1][1].toLowerCase() === myAnswer.toLowerCase()) {
+        //setNumberOfGoodAnswers(count + 1)
+        let numberOfGoodAnswers = localStorage.getItem('numberOfGoodAnswers')
+        numberOfGoodAnswers = Number(numberOfGoodAnswers)+1
+        localStorage.setItem("numberOfGoodAnswers", numberOfGoodAnswers)
+        alert(myAnswer+", correct !"+"\n"+numberOfGoodAnswers+"/"+quizSettings.total_number+" corrects")
+      } else {
+        alert("Nope... "+quizQandA[quizSettings.question_number-1][1]+", not "+myAnswer+"...")
+      }
+      setMyAnswer('')
+      setQuizSettings({...quizSettings, question_number: quizSettings.question_number + 1})
+      if (quizSettings.question_number == quizSettings.total_number) {
+        let numberOfGoodAnswers = localStorage.getItem('numberOfGoodAnswers')
+        let numberOfRequestedClues = localStorage.getItem('numberOfRequestedClues')
+        const resultsData = {
+          maxScore: (quizSettings.total_number * quizSettings.xpPerGoodAnswer),
+          score: (numberOfGoodAnswers * quizSettings.xpPerGoodAnswer) - (numberOfRequestedClues * quizSettings.xpCostPerClue)
+        }
+        alert("Number of good answers: "+numberOfGoodAnswers+"\n"+"Number of requested clues: "+numberOfRequestedClues+"\n"+resultsData.score+" / "+resultsData.maxScore)
+          window.location.href = "/"
+      }
     }
-    setQuizSettings({...quizSettings, question_number: quizSettings.question_number + 1})
-    if (quizSettings.question_number == quizSettings.total_number) {
-        window.location.href = "/"
-    }
-  }
 
   if (!quizSettings) {
     return <div>Loading...</div>;
@@ -88,13 +108,13 @@ const QuizContainer = () => {
         <br/>
         {/*si quiz de type 'image'...*/}
         {quizSettings.type === 'image' && (
-        <>
-            <img style={{ display: 'inline-block' }} src={quizSettings.imageUrl} alt={quizSettings.imageAlt} width={250} height={"auto"} />
-            <p>
-                <br/><br/>
-                Question {quizSettings.question_number} of {quizSettings.total_number}
-            </p>
-        </>
+          <>
+              <img style={{ display: 'inline-block' }} src={quizSettings.imageUrl} alt={quizSettings.imageAlt} width={250} height={"auto"} />
+              <p>
+                  <br/><br/>
+                  Question {quizSettings.question_number} of {quizSettings.total_number}
+              </p>
+          </>
         )}
         {/*si quiz de type 'text'...*/}
         {quizSettings.type === 'text' && (
@@ -107,12 +127,13 @@ const QuizContainer = () => {
         <br/>
         {/*si réponse de type 'text'...*/}
         {quizSettings.response_type === 'text' && (
-          <input style={{width: 300, height: 40, border: "solid", borderWidth: "1px", borderRadius: "10px", marginTop: "20px", marginBottom: "50px", padding: "0 5px"}} placeholder="Try your best!..." onChange={(event) => setMyAnswer(event.target.value)} />
+          <input style={{width: 300, height: 40, border: "solid", borderWidth: "1px", borderRadius: "10px", marginTop: "20px", marginBottom: "50px", padding: "0 5px"}} placeholder="Try your best!..." value={myAnswer} onChange={(event) => setMyAnswer(event.target.value)} />
         )}
         <br/>
         {showClue === true ? (
-            <span>Clue: {clue}...</span>)
-           : (<button style={{fontStyle: "italic", border: "solid", borderWidth: "1px", borderRadius: "10px", padding: "0 15px"}} onClick={handleClue}>Pay {"{"}clue_price{"}"} xp for a clue !</button>
+            <span>Clue: {clue}</span>
+            ) : (
+            <button style={{fontStyle: "italic", border: "solid", borderWidth: "1px", borderRadius: "10px", padding: "0 15px"}} onClick={handleClue}>Pay {"{"}clue_price{"}"} xp for a clue !</button>
           )
         }
          <button style={{fontStyle: "italic", border: "solid", borderWidth: "1px", borderRadius: "10px", marginLeft: "50px", marginBottom: "50px", padding: "5px 15px", fontStyle: "thick", color: "gray", backgroundColor: "#FFD700"}} onClick={handleResponse} >Answer</button>
