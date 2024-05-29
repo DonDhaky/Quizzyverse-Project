68cc4140276ea7cae712812f9c4f8a17e6aef1e9
AlexandreWilliam <marc-alexandre.lepinard@epitech.eu>
2024-05-29 10:46:26 +0200

    DEL Quiz_Test (replaced by Quiz_Capitals, name changed, code remains the same)
    	deleted:    quizzyverse/src/app/api/ressources/Quiz_Test/route.js
    	deleted:    quizzyverse/src/app/api/ressources/Quiz_Test/settings.json

--- quizzyverse/src/app/api/ressources/Quiz_Test/route.js
+++
@@ -1,8 +0,0 @@
-import {NextResponse} from "next/server"
-import quizSettings from "./settings.json"
-
-export async function GET(request, context){
-    return NextResponse.json({
-        quizSettings,
-    });
-}
