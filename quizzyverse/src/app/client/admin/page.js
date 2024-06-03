0780376bac663a7c3d524ffc7fe4bdfaf4db25de
AlexandreWilliam <marc-alexandre.lepinard@epitech.eu>
2024-06-03 17:50:24 +0200

    UPD admin panel: quiz creation started, ability to write the settings.json (to set up a quiz) in a basic form
    	modified:   quizzyverse/package.json
    	new file:   quizzyverse/src/app/api/write_file/route.js
    	modified:   quizzyverse/src/app/client/admin/page.js

--- quizzyverse/src/app/client/admin/page.js
+++ quizzyverse/src/app/client/admin/page.js
@@ -2,11 +2,16 @@
 
 import { useState, useEffect } from 'react'
 import './admin.css'
+import NavBar from "/src/app/components/Navbar";
 
 const AdminPanel = () => {
 
     const [users, setUsers] = useState([])
     const [userToUpdate, setUserToUpdate] = useState([{}])
+
+    const [content, setContent] = useState(
+        `{\n\t"quiz_title": "Quiz - Find capitals",\n\t"question": "What is the capital of the following country?",\n\t"question_number": 1,\n\t"total_number": 10,\n\t"type": "text",\n\t"api_path": "https://api.allorigins.win/get?url=https://api.worldbank.org/v2/country/?format=json&per_page=296",\n\t"response_obj": "",\n\t"imageUrl": "https://m.media-amazon.com/images/I/71CsUmAR-GL._SL1500_.jpg",\n\t"imageAlt": "",\n\t"show_clue": false,\n\t"clue": "C'est poilu",\n\t"clue_price": 1,\n\t"response_type": "text",\n\t"xpPerGoodAnswer": 2,\n\t"xpCostPerClue": 1\n}`
+    )
 
     const handleInputChange = (event, index, field) => {
         const newUserToUpdate = [...userToUpdate]
@@ -90,26 +95,57 @@ const AdminPanel = () => {
         }
     }
 
+    //////////////////////////////////
+    // Write file
+    const writeFile = async(quizName, fileName, content) => {
+        console.log(content);
+        const dataObj = {quizName: quizName, fileName: fileName, content: content};
+        try {
+            const response = await fetch(
+                '/api/write_file',
+                {
+                    method: 'POST',
+                    headers: {'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(dataObj)
+            })
+            const data = await response.json()
+            console.log(data.message);
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
     return (
         <>
+            <div style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
+                <NavBar style={{width: "100%"}} />
+            </div>
             <div>
                 <div class="divh1" style={{backgroundColor: "orange"}}>
                     <h1>REGISTERED USERS</h1>
                 </div>
+                <br/>
+                <div class="divh2" style={{backgroundColor: "purple"}}>
+                    <h1>user list</h1>
+                </div>
                 <div class="divtable">
                     <table>
-                        <tr>
-                            <th style={{borderTopLeftRadius: "10px"}}>id</th>
-                            <th>is_admin</th>
-                            <th>username</th>
-                            <th>country</th>
-                            <th>email</th>
-                            <th>xp</th>
-                            <th>is_premium</th>
-                            <th>daily_count</th>
-                            <th>renewed_at</th>
-                            <th style={{width: "160px", borderTopRightRadius: "10px"}}>action</th>
-                        </tr>
+                        <thead>
+                            <tr>
+                                <th style={{borderTopLeftRadius: "10px"}}>id</th>
+                                <th>is_admin</th>
+                                <th>username</th>
+                                <th>country</th>
+                                <th>email</th>
+                                <th>xp</th>
+                                <th>is_premium</th>
+                                <th>daily_count</th>
+                                <th>renewed_at</th>
+                                <th style={{width: "160px", borderTopRightRadius: "10px"}}>action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
                         {users.map((user, index) => (
                             <tr key={user.id}>
                                 <td style={index === users.length-1 ? {borderBottomLeftRadius: "10px"} : {}}>{user.id}</td>
@@ -124,6 +160,7 @@ const AdminPanel = () => {
                                 <td style={index === users.length-1 ? {borderBottomRightRadius: "10px"} : {}}><button style={{backgroundColor: "green"}} onClick={() => {getUsers(user.id)}}>&nbsp;Modify&nbsp;</button>&nbsp;&nbsp;{user.is_admin === 0 ? (<>&nbsp;&nbsp;<button style={{backgroundColor: "red"}} onClick={() => {deleteUser(user.id)}}>&nbsp;Delete&nbsp;</button></>) : null}</td>
                             </tr>
                         ))}
+                        </tbody>
                     </table>
                 </div>
                 <div class="divh2" style={{backgroundColor: "purple"}}>
@@ -131,34 +168,48 @@ const AdminPanel = () => {
                 </div>
                 <div class="divtable">
                     <table>
-                        <tr>
-                            <th style={{borderTopLeftRadius: "10px"}}>id</th>
-                            <th>is_admin</th>
-                            <th>username</th>
-                            <th>country</th>
-                            <th>email</th>
-                            <th>xp</th>
-                            <th>is_premium</th>
-                            <th>daily_count</th>
-                            <th>renewed_at</th>
-                            <th style={{width: "160px", borderTopRightRadius: "10px"}}>action</th>
-                        </tr>
-                        {userToUpdate.map((utu, index) => (
-                        <tr key={utu.id}>
-                            <td style={{borderBottomLeftRadius: "10px"}}>{utu.id}</td>
-                            <td><input style={{width: "75px"}} value={utu.is_admin} onChange={(event) => handleInputChange(event, index, 'is_admin')}></input></td>
-                            <td><input style={{width: "75px"}} value={utu.username} onChange={(event) => handleInputChange(event, index, 'username')}></input></td>
-                            <td><input style={{width: "120px"}} value={utu.country} onChange={(event) => handleInputChange(event, index, 'country')}></input></td>
-                            <td><input style={{width: "150px"}} value={utu.email} onChange={(event) => handleInputChange(event, index, 'email')}></input></td>
-                            <td><input style={{width: "100px"}} value={utu.xp} onChange={(event) => handleInputChange(event, index, 'xp')}></input></td>
-                            <td><input style={{width: "80px"}} value={utu.is_premium} onChange={(event) => handleInputChange(event, index, 'is_premium')}></input></td>
-                            <td><input style={{width: "80px"}} value={utu.daily_count} onChange={(event) => handleInputChange(event, index, 'daily_count')}></input></td>
-                            <td><input style={{width: "125px"}} value={utu.renewed_at} onChange={(event) => handleInputChange(event, index, 'renewed_at')}></input></td>
-                            <td style={{borderBottomRightRadius: "10px"}}><button style={{width: "80px", backgroundColor: "DarkCyan"}} onClick={() => {updateUser(userToUpdate)}}>&nbsp;Update&nbsp;</button></td>
-                        </tr>
-                        ))}
+                        <thead>
+                            <tr>
+                                <th style={{borderTopLeftRadius: "10px"}}>id</th>
+                                <th>is_admin</th>
+                                <th>username</th>
+                                <th>country</th>
+                                <th>email</th>
+                                <th>xp</th>
+                                <th>is_premium</th>
+                                <th>daily_count</th>
+                                <th>renewed_at</th>
+                                <th style={{width: "160px", borderTopRightRadius: "10px"}}>action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {userToUpdate.map((utu, index) => (
+                            <tr key={utu.id}>
+                                <td style={{borderBottomLeftRadius: "10px"}}>{utu.id}</td>
+                                <td><input style={{width: "75px"}} value={utu.is_admin} onChange={(event) => handleInputChange(event, index, 'is_admin')}></input></td>
+                                <td><input style={{width: "75px"}} value={utu.username} onChange={(event) => handleInputChange(event, index, 'username')}></input></td>
+                                <td><input style={{width: "120px"}} value={utu.country} onChange={(event) => handleInputChange(event, index, 'country')}></input></td>
+                                <td><input style={{width: "150px"}} value={utu.email} onChange={(event) => handleInputChange(event, index, 'email')}></input></td>
+                                <td><input style={{width: "100px"}} value={utu.xp} onChange={(event) => handleInputChange(event, index, 'xp')}></input></td>
+                                <td><input style={{width: "80px"}} value={utu.is_premium} onChange={(event) => handleInputChange(event, index, 'is_premium')}></input></td>
+                                <td><input style={{width: "80px"}} value={utu.daily_count} onChange={(event) => handleInputChange(event, index, 'daily_count')}></input></td>
+                                <td><input style={{width: "125px"}} value={utu.renewed_at} onChange={(event) => handleInputChange(event, index, 'renewed_at')}></input></td>
+                                <td style={{borderBottomRightRadius: "10px"}}><button style={{width: "80px", backgroundColor: "DarkCyan"}} onClick={() => {updateUser(userToUpdate)}}>&nbsp;Update&nbsp;</button></td>
+                            </tr>
+                            ))}
+                        </tbody>
                     </table>
                 </div>
+                <br/>
+                <div class="divh1" style={{backgroundColor: "orange"}}>
+                    <h1>QUIZ MANAGEMENT</h1>
+                </div>
+                <br/>
+                <div class="divh2" style={{backgroundColor: "purple"}}>
+                    <h1>create custom quiz</h1>
+                </div>
+                &nbsp;<br/><br/><br/><br/><br/>&nbsp;
+                <button style={{width: "80px", backgroundColor: "DarkCyan"}} onClick={() => {writeFile("myQuiz", "settings.json", content)}}>&nbsp;Write file&nbsp;</button>
             </div>
         </>
     )
