94adb1f400102af26353cbe8cafc8cc6c84153e5
AlexandreWilliam <marc-alexandre.lepinard@epitech.eu>
2024-06-05 14:06:56 +0200

    UPD Link system to custom quizzes
    	new file:   quizzyverse/src/app/api/route.js
    	modified:   quizzyverse/src/app/page.js
    	modified:   quizzyverse/src/app/quiz/page.js

--- quizzyverse/src/app/page.js
+++ quizzyverse/src/app/page.js
@@ -1,23 +1,69 @@
-// src/app/page.js (or wherever your Home component is located)
+"use client"
+
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import NavBar from "./components/Navbar";
 import { Libre_Franklin } from "next/font/google";
 
+const handleQuiz = (event) => {
+  console.log(event);
+  let quizName = event.target.innerHTML.replaceAll(" ", "_")
+  console.log(quizName);
+  localStorage.setItem('quizName', quizName)
+  window.location.href = "/quiz"
+}
+
 export default function Home() {
+  
+  const [customQuizList, setCustomQuizList] = useState([])
+  const [files, setFiles] = useState([]);
+
+  useEffect(() => {
+    const fetchDirectoryList = async () => {
+      try {
+        const response = await fetch('/api');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        setCustomQuizList(data.directories);
+      } catch (error) {
+        console.error('Error fetching directory list:', error);
+      }
+    };
+
+    fetchDirectoryList();
+  }, []);
+
   return (
     <main className="min-h-screen bg-gray-100 flex flex-col items-center p-4">
       <NavBar/>
-      <section className="flex justify-center items-center h-screen">
-        <Link href='/Lolverse' className="bg-blue-600 text-white font-bold py-2 px-4 rounded-lg hover:bg-blue-700 transition-colors duration-300">
+      <section className="flex flex-col justify-center items-center h-screen">
+        <Link href='/Lolverse' className="bg-blue-600 text-white font-bold py-2 px-4 rounded-lg hover:bg-blue-700 transition-colors duration-300 m-5">
           Start Quiz
         </Link>
-        <Link href='/quizz-flag' target="_blank" className="bg-blue-600 text-white font-bold py-2 px-4 rounded-lg hover:bg-blue-700 transition-colors duration-300">
+        <Link href='/quizz-flag' target="_blank" className="bg-blue-600 text-white font-bold py-2 px-4 rounded-lg hover:bg-blue-700 transition-colors duration-300 m-5">
           Start Quiz Flag
         </Link>
-        <Link href='/quizz-cocktail' className="bg-blue-600 text-white font-bold py-2 px-4 rounded-lg hover:bg-blue-700 transition-colors duration-300">
+        <Link href='/quizz-cocktail' className="bg-blue-600 text-white font-bold py-2 px-4 rounded-lg hover:bg-blue-700 transition-colors duration-300 m-5">
           Start Quiz Cocktail
         </Link>
+
+        {customQuizList.map((quiz, index) => (
+          <button key={index} className="bg-violet-500 text-white font-bold py-2 px-4 rounded-lg hover:bg-blue-700 transition-colors duration-300 m-5" onClick={(event) => handleQuiz(event)}>{customQuizList[index].replaceAll('_', ' ')}</button>
+        ))}
+        {files}
       </section>
     </main>
   );
 }
+
+
+/*
+//To see elements stored in localStorage using browser console:
+for (var i = 0; i < localStorage.length; i++){
+    console.log(localStorage.key(i) + ":");
+    console.log(localStorage.getItem(localStorage.key(i)));
+    console.log("\n");
+}
+*/
