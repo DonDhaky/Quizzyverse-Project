64708d5766fb703bc0f3f1823bdea815ed6e3ada
DonDhaky <erick.thonon@epitech.eu>
2024-06-07 10:51:36 +0200

    EDIT premium button

--- quizzyverse/src/app/page.js
+++ quizzyverse/src/app/page.js
@@ -38,7 +38,7 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-gray-100 flex flex-col items-center p-4">
       <NavBar/>
-      <section className="flex flex-col justify-center items-center h-screen">
+      <section className="flex flex-col justify-center items-center mt-10">
         <Link href='/Lolverse' className="bg-blue-600 text-white font-bold py-2 px-4 rounded-lg hover:bg-blue-700 transition-colors duration-300 m-5">
           Start Quiz
         </Link>
